e776a0fccb6f4073c5e4cf35eb52b43e2aa5d88e
DariusMilevicius <Dariuxsa@gmail.com>
2022-03-24 22:06:22 +0200

    pamoka23

---
+++ Pamoka23/index23.js
@@ -0,0 +1,24 @@
+
+
+let myInput = document.querySelector("input");
+myInput.addEventListener("change", function (event){
+    console.log (event.target.value)
+})
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++
+const h1 = document.createElement('h1')
+document.body.appendChild(h1)
+// h1.innerText= "Sveikis"
+
+function addText(event){
+ h1.innerText = `Sveiki  ${myInput.value}`
+
+}
+myInput.addEventListener('input', addText)
+
+// myInput.addEventListener("input", function(event){
+//     h1.innerText = `Sveiki  ${event.target.value}`
+// })
